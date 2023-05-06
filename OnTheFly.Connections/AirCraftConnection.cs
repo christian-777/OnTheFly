de61cf1be2cafb1aa7f5eebaef8acac92cdc99c5
christian-777 <60942806+christian-777@users.noreply.github.com>
2023-05-06 01:32:50 -0300

    criada a conexao com mongo em aircraft

---
+++ OnTheFly.Connections/AirCraftConnection.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Driver;
+using OnTheFly.Models;
+
+namespace OnTheFly.Connections
+{
+    public class AirCraftConnection
+    {
+        public IMongoDatabase Database { get; private set; }
+        public AirCraftConnection()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            Database = client.GetDatabase("AirCraft");
+        }
+
+        public void Insert(AirCraft airCraft)
+        {
+            var collection = Database.GetCollection<AirCraft>("ActiveAirCraft");
+            collection.InsertOne(airCraft);
+        }
+
+        public List<AirCraft> FindAll()
+        {
+            var collection = Database.GetCollection<AirCraft>("ActiveAirCraft");
+            return collection.Find(a=>true).ToList();
+        }
+
+        public void Delete(string rab)
+        {
+            var collection = Database.GetCollection<AirCraft>("ActiveAirCraft");
+            var collection2 = Database.GetCollection<AirCraft>("InactiveAirCraft");
+
+            var trash = collection.Find<AirCraft>(a=>a.RAB==rab).FirstOrDefault();
+
+            collection2.InsertOne(trash);
+
+            collection.FindOneAndDelete(a=> a.RAB==rab);
+        }
+
+        public void Update(string rab, AirCraft airCraft)
+        {
+            var collection = Database.GetCollection<AirCraft>("ActiveAirCraft");
+            collection.ReplaceOne(a => a.RAB == rab, airCraft);
+        }
+    }
+}
