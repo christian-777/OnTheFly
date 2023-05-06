49baf2170932225f9412ae0f920b1150564e7f9d
Luis Guilherme <luislebert01@outlook.com>
2023-05-06 12:30:45 -0300

    construcao da FlightConnection iniciada

---
+++ OnTheFly.Connections/FlightConnection.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Driver;
+using OnTheFly.Models;
+
+namespace OnTheFly.Connections
+{
+    public class FlightConnection
+    {
+        public IMongoDatabase Database { get; private set; }
+        public FlightConnection()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            Database = client.GetDatabase("Flight");
+        }
+
+        public void Insert(Flight flight)
+        {
+            var activeCollection = Database.GetCollection<Flight>("ActiveFlight");
+            activeCollection.InsertOne(flight);
+        }
+
+        public List<Flight> FindAll()
+        {
+            var activeCollection = Database.GetCollection<Flight>("ActiveFlight");
+            return activeCollection.Find(a=>true).ToList();
+        }
+
+        public void Delete(string id)
+        {
+            IMongoCollection<Flight> activeCollection = Database.GetCollection<Flight>("ActiveFlight");
+            IMongoCollection<Flight> inactiveCollection = Database.GetCollection<Flight>("InactiveFlight");
+
+            Flight? trash = activeCollection.FindOneAndDelete(f => f.Id == id);
+
+            if (trash != null) inactiveCollection.InsertOne(trash);
+        }
+
+        public void Update(string id, Flight flight)
+        {
+            var activeCollection = Database.GetCollection<Flight>("ActiveAirCraft");
+            activeCollection.ReplaceOne(f => f.Id  == id, flight);
+        }
+    }
+}
--- OnTheFly.Models/Flight.cs
+++ OnTheFly.Models/Flight.cs
@@ -1,7 +1,13 @@
-﻿namespace OnTheFly.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace OnTheFly.Models
 {
     public class Flight
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
         public Airport Destiny { get; set; }
         public AirCraft Plane { get; set; }
         public int Sales { get; set; }
