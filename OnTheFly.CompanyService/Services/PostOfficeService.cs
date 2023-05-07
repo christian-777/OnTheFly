1b2b331154fceb287457f6b522406802fcd76de9
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-05-07 00:22:14 -0300

    criacao do micro servico onthefly.companyservice e da conexao com o mongo companyconnection

---
+++ OnTheFly.CompanyService/Services/PostOfficeService.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using OnTheFly.Models;
+
+namespace OnTheFly.Services
+{
+    public class PostOfficeService
+    {
+        static readonly HttpClient address = new HttpClient();
+
+        public async Task<Address> GetAddress(string zipCode)
+        {
+            try
+            {
+                HttpResponseMessage response = await address.GetAsync("https://viacep.com.br/ws/" + zipCode + "/json/");
+                response.EnsureSuccessStatusCode();
+                string addressResponse = await response.Content.ReadAsStringAsync();
+                var ad = JsonConvert.DeserializeObject<Address>(addressResponse);
+                return ad;
+            }
+            catch (HttpRequestException e)
+            {
+                throw;
+            }
+        }
+    }
+}
--- OnTheFly.Models/Company.cs
+++ OnTheFly.Models/Company.cs
@@ -6,12 +6,18 @@ namespace OnTheFly.Models
     {
         [StringLength(19)]
         public string Cnpj { get; set; }
+
         [StringLength(30)]
         public string Name { get; set; }
+
         [StringLength(30)]
         public string NameOPT { get; set; }
-        public DateOnly DtOpen { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DtOpen { get; set; }
+
         public bool? Status { get; set; }
+
         public Address Address { get; set; }
     }
 }
